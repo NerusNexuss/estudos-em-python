c20c979b3f13997f2fd9b9da73063f2468a8e5f8
Marcos da Luz <108414450+NerusNexuss@users.noreply.github.com>
2023-02-28 15:10:14 -0300

    Add files via upload
     faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todos as informações possíveis sobre ela

---
+++ estudando.py
@@ -0,0 +1,10 @@
+algo = input('Digite algo: ')
+
+print("O tipo primitivo desse valor é",type(algo))
+print("Só tem espaços?", algo.isspace())
+print("Só tem numero?:", algo.isalnum())
+print("È alfabetico?:", algo.isalpha())
+print("È alfanumérico?:", algo.isalnum())
+print("Está em maiúsculas", algo.isupper())
+print("Está em minusculas", algo.islower)
+
